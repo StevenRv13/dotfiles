0b68bf2c18d079028180c91ba112a4a42ba8e444
StevenRv13 <stevenramirezv@gmail.com>
2026-05-16 01:42:26 -0600

    Setup Linux

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+set fish_color_user ff6600
+set fish_color_host ff6600
+set fish_color_cwd ff6600
+set fish_color_cwd_root ff6600
+set fish_color_valid_path ff6600
+set -x LS_COLORS "di=38;5;208:fi=0:ln=38;5;202:ex=38;5;166"
+fastfetch
